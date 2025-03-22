cea69cf35ed1afa6f9f414d298deaa6e70585a7f
agustin castellon <agustincastellon06@gmail.com>
2025-03-22 20:47:04 -0300

    paginas de capitulos enlazados al servidor

--- src/Services/MangaDex/AtHomeService.jsx
+++ src/Services/MangaDex/AtHomeService.jsx
@@ -1,18 +1,11 @@
 import axios from 'axios'
 
-const BASE_URL = '/api'
 
-export const getChapter = async (chapterId)=>{
-    try{
-        const response = await axios.get(`${BASE_URL}/at-home/server/${chapterId}`)
-        const baseChapterUrl = response.data.baseUrl;
-        const hash = response.data.chapter.hash;
-        return response.data.chapter.dataSaver.map(c =>{
-            return{
-                pageUrl: `${baseChapterUrl}/data-saver/${hash}/${c}`
-            }
-        })
-    } catch(error){
+export const getChapter = async (chapterId) => {
+    try {
+        const response = await axios.get(`/api/chapter/${chapterId}`);
+        return response.data.pages;
+    } catch (error) {
         console.error('Error obteniendo capítulos:', error);
         return [];
     }
